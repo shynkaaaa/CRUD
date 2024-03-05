98b9b465f824f36cbef7f42a325fb0bff018cd95
shynkaaaa <shynkataimasov@gmail.com>
2024-03-05 15:04:33 +0500

    controller actions and new views

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,7 +12,8 @@ class UsersController extends Controller
      */
     public function index()
     {
-        dd(123);
+        $users = User::get();
+        return view('index', compact('users'));
     }
 
     /**
@@ -20,7 +21,7 @@ class UsersController extends Controller
      */
     public function create()
     {
-        //
+        return view('form');
     }
 
     /**
@@ -36,7 +37,7 @@ class UsersController extends Controller
      */
     public function show(User $user)
     {
-        //
+        return view('show');
     }
 
     /**
@@ -44,7 +45,7 @@ class UsersController extends Controller
      */
     public function edit(User $user)
     {
-        //
+        return view('form', compact('user'));
     }
 
     /**
