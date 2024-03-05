ee6b11f336ffa581a322af1dc5c869c123cf41e8
shynkaaaa <shynkataimasov@gmail.com>
2024-03-05 18:33:08 +0500

    card for user

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,7 +10,7 @@ class UsersController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
     {
         $users = User::get();
         return view('index', compact('users'));
@@ -19,7 +19,7 @@ class UsersController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
     {
         return view('form');
     }
@@ -27,7 +27,7 @@ class UsersController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): \Illuminate\Http\RedirectResponse
     {
         User::create($request->only(['name', 'email']));
         return redirect()->route('users.index');
@@ -36,15 +36,15 @@ class UsersController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(User $user)
+    public function show(User $user): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
     {
-        return view('show');
+        return view('show', compact('user'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(User $user)
+    public function edit(User $user): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
     {
         return view('form', compact('user'));
     }
@@ -52,7 +52,7 @@ class UsersController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, User $user)
+    public function update(Request $request, User $user): \Illuminate\Http\RedirectResponse
     {
         $user->update($request->only(['name', 'email']));
         return redirect()->route('users.index');
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,7 +1,18 @@
 @extends('layout')
 
-@section('title', 'Users')
+@section('title', 'User '.$user->name)
 
 @section('content')
-    dd {{ $user }}
+    <a type="button" class="btn btn-secondary mb-2" href="{{ route('users.index') }}">Back to users</a>
+    <div class="card" style="width: 18rem;">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Id: {{$user->id}}</li>
+            <li class="list-group-item">Name: {{$user->name}}</li>
+            <li class="list-group-item">Email: {{$user->email}}</li>
+            <li class="list-group-item">Created: {{$user->created_at->format('d/m/y H:i:s')}}</li>
+            <li class="list-group-item">Updated: {{$user->updated_at->format('d/m/y H:i:s')}}</li>
+        </ul>
+    </div>
+
+    <a href="{{ route('users.edit', $user) }}" type="button" class="btn btn-warning">Edit</a>
 @endsection
